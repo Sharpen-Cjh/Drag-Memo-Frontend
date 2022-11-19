a273e413c7f863f1e7992f930a343be168c24291
Sharpen-Cjh <101804186+Sharpen-Cjh@users.noreply.github.com>
2022-11-20 02:33:57 +0900

    Feat 로그인 기능 구현

--- src/app/Pages.js
+++ src/app/Pages.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import MyMemos from "../pages/MyMemos";
 import Login from "../pages/Login";
 import NotFound from "../components/NotFound";
 import Memo from "../pages/Memo";
 
+import { UserContext } from "../context/userContext";
+
 export default function Pages() {
+  const { loggedInUser } = useContext(UserContext);
+
   return (
     <Routes>
-      <Route path="/" element={<MyMemos />} />
+      <Route
+        path="/"
+        element={loggedInUser ? <MyMemos /> : <Navigate to="/login" />}
+      />
       <Route path="/login" element={<Login />} />
-      <Route path="/memos/:memoId" element={<Memo />} />
+      <Route
+        path="/memos/:memoId"
+        element={loggedInUser ? <Memo /> : <Navigate to="/login" />}
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../config/firebase";
+
+import axiosInstance from "../api/axios";
+import { ERROR } from "../constants/error";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const {
+        user: {
+          displayName,
+          reloadUserInfo: { localId },
+        },
+      } = await signInWithPopup(auth, provider);
+
+      const { status } = await axiosInstance.post("/login", {
+        username: displayName,
+        googleId: localId,
+      });
+
+      status === 200 ? navigate("/") : setErrorMessage(ERROR.FAIL_LOGIN);
+    } catch (error) {
+      setErrorMessage(ERROR.FAIL_LOGIN);
+    }
+  };
+
+  return (
+    <LoginButtonWrapper>
+      <LoginButton onClick={handleLogin}>Login</LoginButton>
+      <div>{errorMessage}</div>
+    </LoginButtonWrapper>
+  );
 }
+
+const LoginButton = styled.button`
+  width: 300px;
+  height: 200px;
+  font-size: 30px;
+`;
+
+const LoginButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
